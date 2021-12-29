74c62bf5f7f15fe4bbc59393bd024656b387a97c
VladTataritsky <Tataritskii@bk.ru>
2021-12-29 17:16:48 +0300

    add splash screen

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
 import React from 'react';
 
 import {Text, View, Button, FlatList} from 'react-native';
+import SplashScreen from 'react-native-splash-screen'
 
 const names = ['Jack', 'John', 'Ann', 'Robert', 'Steve'];
 
 const App = () => {
   React.useEffect(() => {
-    fetch('https://api.github.com/users/xiaotian/repos')
-      .then(
-        resp => resp.json(), // this returns a promise
-      )
-      .then(repos => {
-        for (const repo of repos) {
-          console.log(repo.name);
-        }
-      })
-      .catch(ex => {
-        console.error(ex);
-      });
+    SplashScreen.hide();
   }, []);
   console.log('logging');
   return (
